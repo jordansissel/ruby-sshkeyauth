06662f9ac81e1734cd62970a5079aa82e228fd61
Jordan Sissel <jls@semicomplete.com>
2010-10-11 01:02:02 -0700

    - Throw exception if we try to add a host key that can't be found.

--- lib/ssh/key/helper.rb
+++ lib/ssh/key/helper.rb
@@ -1,5 +1,8 @@
 require "net/ssh"
 
+module SSH; module Key; class KeyNotFound < StandardError
+end; end; end
+
 module SSH; module Key; module Helper
   # Add a private key to this signer.
   def add_key_file(path, passphrase=nil)
@@ -9,7 +12,11 @@ module SSH; module Key; module Helper
 
   # Add a public key from your known_hosts file
   def add_key_from_host(hostname)
-    hostkey = %x{ssh-keygen -F #{identity}}.split("\n")[1].chomp.split(" ",2)[-1]
+    hostkey = %x{ssh-keygen -F "#{hostname}"}.split("\n")[1].chomp.split(" ",2)[-1] rescue nil
+    if hostkey == nil
+      raise SSH::Key::KeyNotFound.new("Could not find host key '#{hostname}' " \
+                                       "in known_hosts (using ssh-keygen -F)")
+    end
     @keys << Net::SSH::KeyFactory.load_data_public_key(hostkey)
   end
 
